677f5864603e392af861b6d5ea44b7350d66de77
Gorbachev Egor <7gorbachevm@gmail.com>
2023-07-22 12:00:49 +0300

    Check code style for the bin folder

--- ecs.php
+++ ecs.php
@@ -9,15 +9,13 @@ use PhpCsFixer\Fixer\Operator\NotOperatorWithSuccessorSpaceFixer;
 use PhpCsFixer\Fixer\Phpdoc\PhpdocLineSpanFixer;
 use PhpCsFixer\Fixer\Strict\DeclareStrictTypesFixer;
 use SlevomatCodingStandard\Sniffs\ControlStructures\EarlyExitSniff;
-use SlevomatCodingStandard\Sniffs\Namespaces\DisallowGroupUseSniff;
-use SlevomatCodingStandard\Sniffs\Namespaces\FullyQualifiedGlobalFunctionsSniff;
-use SlevomatCodingStandard\Sniffs\Namespaces\ReferenceUsedNamesOnlySniff;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
 return function (ECSConfig $ecsConfig): void {
     $ecsConfig->paths([
         __DIR__ . '/src',
+        __DIR__ . '/bin',
         __DIR__ . '/tests',
     ]);
 
