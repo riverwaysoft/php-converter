87e24205d6f050c6989c643dd910ba1a1deec4f7
Gorbachev Egor <7gorbachevm@gmail.com>
2024-04-19 13:39:53 +0700

    upd

--- phpstan-baseline.neon.php
+++ phpstan-baseline.neon.php
@@ -1,8 +1,9 @@
 <?php declare(strict_types = 1);
 
+// https://github.com/phpstan/phpstan/discussions/6604#discussioncomment-2133932
 $includes = [];
 if (PHP_VERSION_ID >= 80200) {
-    $includes[] = __DIR__ . '/baseline-8.1.neon';
+    $includes[] = __DIR__ . '/phpstan-baseline.neon';
 }
 $config = [];
 $config['includes'] = $includes;
