7e342d8814bff3b274bc8176725e76cd9056e4c8
Gorbachev Egor <7gorbachevm@gmail.com>
2024-04-19 13:38:13 +0700

    Conditional baseline

---
+++ phpstan-baseline.neon.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types = 1);
+
+$includes = [];
+if (PHP_VERSION_ID >= 80200) {
+    $includes[] = __DIR__ . '/baseline-8.1.neon';
+}
+$config = [];
+$config['includes'] = $includes;
+
+return $config;
