1f359f6616d23037be31f525f85ea7be84aa5ff2
KoNekoD <108808201+KoNekoD@users.noreply.github.com>
2024-04-23 03:49:41 +0300

    Go converter - Fix enum already exist declaration (#122)

--- tests/OutputGenerator/__snapshots__/GoGeneratorTest/testUseTypeEnum/GoGeneratorTest__testUseTypeEnum__1.go
+++ tests/OutputGenerator/__snapshots__/GoGeneratorTest/testUseTypeEnum/GoGeneratorTest__testUseTypeEnum__1.go
@@ -7,17 +7,19 @@ package gen
 type ColorEnum int
 
 const (
-	RED   ColorEnum = 0
-	GREEN ColorEnum = 1
-	BLUE  ColorEnum = 2
+	RED               ColorEnum = 0
+	GREEN             ColorEnum = 1
+	BLUE              ColorEnum = 2
+	UNIFIED_ENUM_CASE ColorEnum = 111
 )
 
 type RoleEnum string
 
 const (
-	ADMIN  RoleEnum = "admin"
-	READER RoleEnum = "reader"
-	EDITOR RoleEnum = "editor"
+	ADMIN                     RoleEnum = "admin"
+	READER                    RoleEnum = "reader"
+	EDITOR                    RoleEnum = "editor"
+	UNIFIED_ENUM_CASERoleEnum RoleEnum = "one_one_one"
 )
 
 type User struct {
